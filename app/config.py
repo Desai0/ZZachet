879e450a753bf94449f1e265df420d7bc86cd83e
Desai0 <kolobokevgenij83@gmail.com>
2026-04-17 16:32:45 +0300

    добил редис

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
+import os
+
+
 DATABASE_URL = "sqlite:///./app.db"
 SECRET_KEY = "super_secret_key_for_demo_project_change_me"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
-REDIS_URL = "redis://localhost:6379/0"
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
